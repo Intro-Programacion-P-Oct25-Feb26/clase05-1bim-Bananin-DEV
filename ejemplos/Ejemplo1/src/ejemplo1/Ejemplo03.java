889cf930ab272fae1956be191b6b5208ae9b474e
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-11-12 12:01:18 -0500

    Ejemplo 1

--- ejemplos/Ejemplo1/src/ejemplo1/Ejemplo03.java
+++ ejemplos/Ejemplo1/src/ejemplo1/Ejemplo03.java
@@ -11,7 +11,7 @@ import java.util.Locale;
  *
  * @author UTPL
  */
-public class NewClass {
+public class Ejemplo03 {
     public static void main(String[] args) {
         
         // Condicionales simplesmain
